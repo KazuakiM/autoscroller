4339f218797fe9ea89b8fb3387c4d29ea7921b19
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2017-10-29 23:30:22 +0900

    Improve wait scroll

--- src/background.js
+++ src/background.js
@@ -5,13 +5,18 @@ let autoscrollerBS = {
   "timmer": null,
   "interval": 10,
 
+  "isInteger": function(value) {
+    return Math.round(value) === value;
+  },
+
   "checkLSData": function(lsData) {
     //{{{
 
-    if (typeof lsData.intervalLS !== "undefined" && Math.round(parseInt(lsData.intervalLS, 10)) === parseInt(lsData.intervalLS, 10)) {
+    if (typeof lsData.intervalLS !== "undefined" && autoscrollerBS.isInteger(parseInt(lsData.intervalLS, 10))) {
       autoscrollerBS.interval = parseInt(lsData.intervalLS, 10);
     } else {
-      autoscrollerBS.interval = (Math.round(autoscrollerBS.interval) === autoscrollerBS.interval) ? autoscrollerBS.interval : 10;
+      autoscrollerBS.interval = (autoscrollerBS.isInteger(autoscrollerBS.interval)) ? autoscrollerBS.interval : 10;
+
       autoscrollerBS.ls.set({
         "intervalLS": autoscrollerBS.interval
       });
@@ -21,34 +26,61 @@ let autoscrollerBS = {
     //}}}
   },
 
-  "sendMessage": function(tab) {
+  "stopTimmer": function(tabId) {
     //{{{
     clearInterval(autoscrollerBS.timmer);
+    autoscrollerBS.timmer = null;
 
+    browser.browserAction.setIcon({
+      "tabId": tabId,
+      "path": "img/dead.svg"
+    });
+    //}}}
+  },
+
+  "makeConnection": function(tab) {
+    //{{{
     if (autoscrollerBS.timmer !== null) {
-      browser.browserAction.setIcon({
-        "tabId": tab.id,
-        "path": "img/dead.svg"
-      });
-
-      autoscrollerBS.timmer = null;
-
+      autoscrollerBS.stopTimmer(tab.id);
     } else {
       browser.browserAction.setIcon({
         "tabId": tab.id,
         "path": "img/active.svg"
       });
 
-      autoscrollerBS.timmer = setInterval(function() {
-        browser.tabs.sendMessage(tab.id, {});
-      }, autoscrollerBS.ls.get().then(autoscrollerBS.checkLSData, autoscrollerBS.onLSError));
+      browser.tabs.sendMessage(tab.id, {}).then(function() {
+        autoscrollerBS.useMessage(tab.id);
+      }).catch(function() {
+        let errorCnt = 0
+        clearInterval(autoscrollerBS.timmer);
+        autoscrollerBS.timmer = setInterval(function() {
+          if (errorCnt < 10) {
+            browser.tabs.sendMessage(tab.id, {}).then(function() {
+              autoscrollerBS.useMessage(tab.id);
+            }).catch(function() {
+              ++errorCnt;
+            });
+          } else {
+            autoscrollerBS.stopTimmer(tab.id);
+          }
+        }, 2000);
+      });
     }
     //}}}
   },
 
-  "onLSError": function(e) {
+  "useMessage": function(tabId) {
+    //{{{
+    clearInterval(autoscrollerBS.timmer);
+    autoscrollerBS.timmer = setInterval(function() {
+      browser.tabs.sendMessage(tabId, {});
+    }, autoscrollerBS.ls.get().then(autoscrollerBS.checkLSData, autoscrollerBS.onLSError));
+    //}}}
+  },
+
+  "onLSError": function() {
     return 10;
   }
 };
 
-browser.browserAction.onClicked.addListener(autoscrollerBS.sendMessage);
+browser.browserAction.onClicked.addListener(autoscrollerBS.makeConnection);
